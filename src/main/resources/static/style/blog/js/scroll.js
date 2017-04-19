62bddebfe60f34ea04264c60eb57e13ba29ef56b
Your Name <zhanglijie1995@163.com>
2017-04-19 17:15:01 +0800

    blog前端样式改变

--- src/main/resources/static/style/blog/js/scroll.js
+++ src/main/resources/static/style/blog/js/scroll.js
@@ -1,14 +1,15 @@
-var pageNumber = 0;
+var pageNumber = 1;
 
 $(window).scroll(function () {
     //下面这句主要是获取网页的总高度，主要是考虑兼容性所以把Ie支持的documentElement也写了，这个方法至少支持IE8
     var htmlHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
     //clientHeight是网页在浏览器中的可视高度，
-    var clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
+    var clientHeight =  document.documentElement.clientHeight;
     //scrollTop是浏览器滚动条的top位置，
     var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
     //通过判断滚动条的top位置与可视网页之和与整个网页的高度是否相等来决定是否加载内容；
-    if (scrollTop + clientHeight + 10 == htmlHeight + 10) {
+    if (htmlHeight-scrollTop - clientHeight <10 ) {
+        alert(1)
         extracted();
     }
 })
@@ -18,8 +19,17 @@ function extracted() {
     if ($('#serch').val() != null && $('#serch').val() != "") {
         path = "/article/serch";
     }
-    $.get(path, {content: "%" + $('#serch').val() + "%", pageNumber: ++pageNumber,user:$("#userId").val()}, function (data) {
-        $('#content').append(data);
+    $.get(path, {content: "%" + $('#serch').val() + "%", pageNumber:pageNumber,user:$("#userId").val()}, function (data) {
+        if(data!=""){
+            $('.blog-main-left').append(data);
+            pageNumber++;
+        }
     });
+}
 
+function serch() {
+    $.get("/article/serch", {content: "%" + $('#serch').val() + "%", pageNumber: 0}, function (data) {
+        pageNumber = 0;
+        $(".blog-main-left").replaceWith("<div class='blog-main-left'>" + data + "</div>");
+    })
 }
